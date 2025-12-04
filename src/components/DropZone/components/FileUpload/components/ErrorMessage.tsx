5c84a2c034ec26b9f972822ba60f8884cec59e6d
whizzkid1452 <whizzkid1452@gmail.com>
2025-12-04 11:20:08 +0900

    에러메시지지

---
+++ src/components/DropZone/components/FileUpload/components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+import * as styles from '../FileUpload.css';
+
+interface ErrorMessageProps {
+  message: string;
+}
+
+export function ErrorMessage({ message }: ErrorMessageProps) {
+  return <div className={styles.errorMessage}>{message}</div>;
+}
+
+
+
