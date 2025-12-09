dad1fc2e590fa9f61092db0f622580b7eb69b806
whizzkid1452 <whizzkid1452@gmail.com>
2025-12-09 17:47:19 +0900

    레이지로딩 제거

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,29 +1,8 @@
-import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { DefaultLayout } from '@/components/Layouts/DefaultLayout';
 import { HomePage } from '@/components/Home/HomePage';
 import { DropZonePage } from '@/components/DropZone/DropZonePage';
-
-// DAW 페이지만 lazy load (wavesurfer.js 등 무거운 의존성 포함)
-const DawPage = lazy(() => import('@/components/Daw/DawPage'));
-
-// Loading fallback 컴포넌트
-const LoadingFallback = () => (
-  <DefaultLayout>
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '50vh',
-        fontSize: '1.2rem',
-        color: '#666',
-      }}
-    >
-      Loading...
-    </div>
-  </DefaultLayout>
-);
+import { DawPage } from '@/components/Daw/DawPage';
 
 // View 컴포넌트들
 const HomeView = () => (
@@ -40,9 +19,7 @@ const DropZoneView = () => (
 
 const DawView = () => (
   <DefaultLayout>
-    <Suspense fallback={<LoadingFallback />}>
-      <DawPage />
-    </Suspense>
+    <DawPage />
   </DefaultLayout>
 );
 
