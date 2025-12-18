3ae26a11cee0008987ae0e9ff19b29e7b40e401e
steinjun0 <1142308@naver.com>
2025-12-19 02:15:38 +0900

    feat: use-audio-file-store 구현

---
+++ src/stores/use-audio-file-store.ts
@@ -0,0 +1,43 @@
+import type { AudioFile } from '@/components/Daw/components/FileUpload/components/types';
+import { create } from 'zustand';
+
+interface AudioFileStore {
+  audioFiles: Map<string, AudioFile>;
+  getAudioFile: ({ url }: { url: string }) => AudioFile | undefined;
+  getAudioFiles: () => Map<string, AudioFile>;
+  addAudioFile: ({
+    url,
+    audioFile,
+  }: {
+    url: string;
+    audioFile: AudioFile;
+  }) => void;
+  removeAudioFile: ({ url }: { url: string }) => void;
+}
+
+export const useAudioFileStore = create<AudioFileStore>()((set, get) => ({
+  audioFiles: new Map(),
+  getAudioFiles: () => {
+    return get().audioFiles;
+  },
+  getAudioFile: ({ url }) => {
+    return get().audioFiles.get(url);
+  },
+  addAudioFile: ({ url, audioFile }) => {
+    set(state => {
+      return {
+        audioFiles: { ...state.audioFiles, [url]: audioFile },
+      };
+    });
+  },
+  removeAudioFile: ({ url }) => {
+    set(state => {
+      const hasExisted = state.audioFiles.delete(url);
+      if (hasExisted) {
+        /** @note 메모리 누수 방지를 위한 방어 코드 */
+        URL.revokeObjectURL(url);
+      }
+      return {};
+    });
+  },
+}));
