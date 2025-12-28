e49576f994a7f701e2867c74828e822eae31d229
steinjun0 <1142308@naver.com>
2025-12-28 12:53:10 +0900

    feat: trackControlV2로 볼륨 제어 추가 및 wavesurferInstance state로 변경

--- src/components/Daw/components/TrackList.tsx
+++ src/components/Daw/components/TrackList.tsx
@@ -1,22 +1,35 @@
 import { useTrackStore } from '@/stores/useTrackStore';
 import WavesurferPlayer from '@wavesurfer/react';
-import { useMemo, useRef } from 'react';
+import { useMemo, useState } from 'react';
 import type WaveSurfer from 'wavesurfer.js';
+import { TrackControlsV2 } from './Track/components/TrackControlsV2';
 import * as styles from './TrackList.css';
 
 export function TrackList() {
   const tracks = useTrackStore(state => state.tracks);
   const trackArray = useMemo(() => Array.from(tracks.values()), [tracks]);
 
-  const wavesurferInstances = useRef<Map<string, WaveSurfer>>(new Map());
+  const [wavesurferInstances, setWavesurferInstances] = useState<
+    Map<string, WaveSurfer>
+  >(new Map());
 
   return (
     <div className={styles.trackList}>
       {/* @todo: 추후 디자인 수정 예정 */}
       <button
         onClick={() => {
-          wavesurferInstances.current.forEach(ws => {
-            ws.play();
+          wavesurferInstances.forEach(ws => {
+            // ws.play();
+            // ws는 visualize만 책임지기 때문에 WebAudioApi로 직접 재생합니다
+
+            const mediaElement = ws.getMediaElement();
+            mediaElement.play();
+            const audioContext = new AudioContext();
+            const source = audioContext.createMediaElementSource(mediaElement);
+            const panner = audioContext.createStereoPanner();
+            panner.pan.value = 1;
+            source.connect(panner);
+            panner.connect(audioContext.destination);
           });
         }}
       >
@@ -24,8 +37,11 @@ export function TrackList() {
       </button>
       <button
         onClick={() => {
-          wavesurferInstances.current.forEach(ws => {
-            ws.pause();
+          wavesurferInstances.forEach(ws => {
+            // ws.pause();
+            // ws는 visualize만 책임지기 때문에 WebAudioApi로 직접 재생합니다
+            const mediaElement = ws.getMediaElement();
+            mediaElement.pause();
           });
         }}
       >
@@ -33,29 +49,44 @@ export function TrackList() {
       </button>
       <div className={styles.tracksContainer}>
         {trackArray.map(track => {
+          const thisWs = wavesurferInstances.get(track.id);
           return (
-            <WavesurferPlayer
-              key={track.id}
-              url={track.regions[0].audioFile.url}
-              onReady={ws => {
-                // trackStore에 wavesurfer 인스턴스 저장
-                wavesurferInstances.current.set(track.id, ws);
-              }}
-              onClick={wavesurfer => {
-                wavesurferInstances.current.forEach(ws => {
-                  if (ws === wavesurfer) {
-                    return;
-                  }
-                  if (ws.getCurrentTime() === wavesurfer.getCurrentTime()) {
-                    return;
-                  }
-                  ws.setTime(wavesurfer.getCurrentTime());
-                });
-              }}
-              dragToSeek={true}
-              minPxPerSec={3}
-              width={(track.regions[0].audioFile.duration ?? 1) * 3.1}
-            />
+            <>
+              <WavesurferPlayer
+                key={track.id}
+                url={track.regions[0].audioFile.url}
+                onReady={ws => {
+                  // trackStore에 wavesurfer 인스턴스 저장
+                  setWavesurferInstances(prev => {
+                    const newMap = new Map(prev);
+                    newMap.set(track.id, ws);
+                    return newMap;
+                  });
+                }}
+                onClick={wavesurfer => {
+                  wavesurferInstances.forEach(ws => {
+                    if (ws === wavesurfer) {
+                      return;
+                    }
+                    if (ws.getCurrentTime() === wavesurfer.getCurrentTime()) {
+                      return;
+                    }
+                    ws.setTime(wavesurfer.getCurrentTime());
+                  });
+                }}
+                dragToSeek={true}
+                minPxPerSec={3}
+                width={(track.regions[0].audioFile.duration ?? 1) * 3.1}
+              />
+              {thisWs ? (
+                <TrackControlsV2
+                  initialVolume={thisWs.getVolume()}
+                  onVolumeChange={vol => {
+                    thisWs.setVolume(vol);
+                  }}
+                />
+              ) : null}
+            </>
           );
         })}
       </div>
